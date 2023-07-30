0fbcc4940039fa802e21fee9c8a73d40501793b9
安樂星輝 <toshiki.a1122@gmail.com>
2023-07-30 10:08:47 +0900

    初期設定 & Laravel IDE Helperのインストール

--- backend/config/logging.php
+++ backend/config/logging.php
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily', 'stderr'],
             'ignore_exceptions' => false,
         ],
 
