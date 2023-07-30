5b4c485c7f88f9fc02fa4d476e25ba48a92527a1
安樂星輝 <toshiki.a1122@gmail.com>
2023-07-30 11:20:18 +0900

    データベースクリアSeederの作成

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,22 +1,24 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            // シーダー実行前に全テーブルをトランケート
+            TruncateAllTables::class,
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            // db:seeder実行時に以下のSeederを実行
+            //UserSeeder::class,
+        ]);
     }
 }
